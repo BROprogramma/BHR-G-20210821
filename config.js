896eafba9e7d125f91017dbe5a56a70dca6ad913
Arnoud de Boer <a.deboer@geonovum.nl>
2023-06-19 08:21:06 +0200

    upload 20210821 versie

---
+++ config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  subtitle: "Geologische boormonsterbeschrijving en boormonsteranalyse",
+  //publishDate: "2023-05-09",
+  maxTocLevel: 4,
+ logos: [{
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 776,
+    height: 163,
+    id: 'logo_minbzk',
+  }]  ,	
+  editors: [
+    {
+      name: "Geonovum",
+      url: "https://www.geonovum.nl", 
+    },  
+  ],
+  authors: [
+    {
+      name: "Geonovum",
+	  url: "https://www.geonovum.nl",
+	}],
+  shortName: "bhr-g",
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/BHR-G",
+  previousPublishDate: "2023-05-09",
+  previousStatus: "GN-VV",
+};
